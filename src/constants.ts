3f4d3cd49c6303776b2da6db6d9326c06098f55d
[dyad] <joaquimcruz3110@gmail.com>
2026-01-06 12:32:36 -0300

    [dyad] Atualizada a imagem de todos os produtos do cardápio para a nova imagem enviada. - wrote 2 file(s), executed 1 SQL queries

--- src/constants.ts
+++ src/constants.ts
@@ -52,7 +52,7 @@ export const SEED_MENU_ITEMS = [
     name: 'X-Salada Especial',
     description: 'Pão, hambúrguer, queijo, alface, tomate e maionese especial.',
     price: 22.00,
-    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400&h=400&fit=crop',
+    image: '/assets/default_product.webp',
     available: true,
     stock: 100
   },
@@ -62,7 +62,7 @@ export const SEED_MENU_ITEMS = [
     name: 'XT Monstro',
     description: 'Pão gigante, 2 carnes, ovo, bacon, presunto, queijo, calabresa, alface e tomate.',
     price: 38.00,
-    image: 'https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?w=400&h=400&fit=crop',
+    image: '/assets/default_product.webp',
     available: true,
     stock: 30
   },
@@ -72,7 +72,7 @@ export const SEED_MENU_ITEMS = [
     name: 'Suco de Laranja 500ml',
     description: 'Suco natural feito na hora.',
     price: 12.00,
-    image: 'https://images.unsplash.com/photo-1600266172704-5f7253457173?w=400&h=400&fit=crop',
+    image: '/assets/default_product.webp',
     available: true,
     stock: 50
   }
